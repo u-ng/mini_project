d8ef45ef48a8dde0397cebd5e4c0f61e70b620f5
u-ng <114720707+u-ng@users.noreply.github.com>
2025-02-24 10:18:38 +0900

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -7,9 +7,15 @@ import time
 import requests
 import os # 폴더 생성
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # 헤드리스 모드
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--remote-debugging-port=9222")  # 포트 지정
+
 start_point = "서울기술교육센터"
 url = f"https://map.naver.com/p/search/%EC%84%9C%EC%9A%B8%EA%B8%B0%EC%88%A0%EA%B5%90%EC%9C%A1%EC%84%BC%ED%84%B0?c=15.00,0,0,0,dh"
-driver = wb.Chrome()
+driver = wb.Chrome(options=chrome_options)
 driver.get(url)
 time.sleep(1)
 
